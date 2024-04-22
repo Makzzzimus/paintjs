71ce999f6d41672a90560c1120cc0388b7bd7145
Makzzzimus <2009katita@gmail.com>
2024-04-22 18:15:45 +0300

    Color library

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 let selectedTool = null;
 let paintBrushStroke = 12;
+let selectedColorPicker = document.getElementById("SelectedColor");
+let selectedColorValue = document.getElementById("SelectedColorHex");
 function selectTool(t){
     if (selectedTool === null){
         document.getElementById("ToolPreferencesBox").style.opacity = 1;
         document.getElementById("ToolPreferencesBox").style.transform = "scale(1)";
     }
     else{
-        reverseButtonColor();
+        reverseToolButtonColor();
     }
     selectedTool = t.id.slice(0,3);
-    changeButtonColor(t);
+    changeToolButtonColor(t);
     if(selectedTool == "PBr"){
-        document.getElementById("ToolPreferencesFieldset").innerHTML = `<legend>Tool Settings</legend> <div> <label>Selected width: </label><br> <input id="PBrStrokeSlider" type="range" min="1" max="72" value="${paintBrushStroke}" oninput="changeStrokeSlider(this)"> <input id="PBrStrokeValue" type="number" min="1" max="72" value="${paintBrushStroke}" onchange="changeStrokeNumber(this)"><br> </div> <div> <label>Selected brush type: </label><br> <select id="SelectBrushType"> <option value="">Square</option> </select> </div>`;
+        document.getElementById("ToolPreferencesFieldset").innerHTML = `<legend>Tool Settings</legend> <div> <label>Selected width: </label><br> <input id="PBrStrokeSlider" type="range" min="1" max="72" value="${paintBrushStroke}" oninput="changeStroke(this)"> <input id="PBrStrokeValue" type="number" min="1" max="72" value="${paintBrushStroke}" onchange="changeStroke(this)"><br> </div> <div> <label>Selected brush type: </label><br> <select id="SelectBrushType"> <option value="">Square</option> </select> </div>`;
     }
 }
 
-function changeStrokeSlider(t){
-    document.getElementById(`${selectedTool}StrokeValue`).value = t.value;
+function changeStroke(t){
+    if (t.id.slice(-5) == "Value"){
+        document.getElementById(`${selectedTool}StrokeSlider`).value = t.value;
+    }
+    else{
+        document.getElementById(`${selectedTool}StrokeValue`).value = t.value;
+    }
     if (selectedTool == "PBr"){
         paintBrushStroke = t.value
     }
 }
-function changeStrokeNumber(t){
-    document.getElementById(`${selectedTool}StrokeSlider`).value = t.value;
-    if (selectedTool == "PBr"){
-        paintBrushStroke = t.value
-    }
-}
-function changeButtonColor(t){
+function changeToolButtonColor(t){
     const ELEMENTS_ARRAY = document.querySelectorAll(`.${t.id.slice(0,3)}SvgElement`);
     for(i=0; i<ELEMENTS_ARRAY.length; i++){
         console.log(ELEMENTS_ARRAY)
         ELEMENTS_ARRAY[i].style.fill = "#682375";
     }
 }
-function reverseButtonColor(){
+function reverseToolButtonColor(){
     const ELEMENTS_ARRAY = document.querySelectorAll(`.${selectedTool}SvgElement`);
     for(i=0; i<ELEMENTS_ARRAY.length; i++){
         console.log(ELEMENTS_ARRAY)
         ELEMENTS_ARRAY[i].style.fill = "#9A949B";
     }
+}
+
+
+function setColorFromLibrary(t){
+    selectedColorPicker.value = t.getAttribute("hex-data");
+    selectedColorValue.value = t.getAttribute("hex-data");
 }
